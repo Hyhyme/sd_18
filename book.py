3554fd117c23e9becc3e3b5ef2f7cf67c62b4118
Edward Luo <eluo1@stuy.edu>
2018-04-30 23:57:32 -0400

    finished work

--- book.py
+++ book.py
@@ -4,10 +4,21 @@ data = urllib2.urlopen( "http://www.gutenberg.org/cache/epub/5200/pg5200.txt" ).
 
 book = data.split()
 
-def frequency( x ):
-    ans = 0
-    reduce( lambda a: ans + 1 if a == x, book )
-    return ans
+def frequency( word ):
+    return len( [ x for x in book if x == word ] )
+print( 'Frequency of "Gregor":' )
+print frequency( 'Gregor' ) 
+print( 'Frequency of "the":' )
+print frequency( 'the' ) 
 
-frequency( 'Gregor' )
+def groupFreq( group ):
+    return reduce( lambda a, b: a+b, [frequency(a) for a in group] )
 
+print( 'Frequency of "Gregor" and "the":' )
+print groupFreq( ['Gregor', 'the'] ) 
+
+def most():
+    return reduce( lambda a, b: b if frequency(b) > frequency(a) else a, book )
+
+print( 'Most frequent word:' )
+print most()
